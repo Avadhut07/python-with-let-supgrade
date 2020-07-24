8b32ba91a65285c47218cdafb1a1ce8e2e556562
Avadhut07 <68601790+Avadhut07@users.noreply.github.com>
2020-07-24 23:38:25 +0530

    Add files via upload

---
+++ ASSINGNMENT NO. 5.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[54]:
+
+
+# ASSIGNMENT NO. 5
+#QUASTION NO.1
+# SORT THE INCRESING ORDER BUT ZEROES SHOULD BE A RIGHT HAND SIGHT
+
+lst = [0,1,2,10,4,1,0,56,2,0,1,3,0,56,0,4] 
+n = len(arr) 
+lst.sort()
+
+pushZerosToEnd(lst, n) 
+
+def pushZerosToEnd(lst, n): 
+    count = 0  
+    for i in range(n): 
+        if lst[i] != 0: 
+            lst[count] = arr[i] 
+            count+=1
+    while count < n: 
+        lst[count] = 0
+        count += 1
+print(lst)
+
+
+# In[ ]:
+
+
+# QUASTION NO.2
+
+#Hi sir, 
+#i can't understand the second quastion plz explain me. 
+#i did not have any base of programing language. plz sir tell me how can i solve it.
+# My requaist to you all student does not have the base of language plz. explain the complecate things iin deep.
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
